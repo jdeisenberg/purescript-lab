3b711739ee799285cf91254707223c65d9727df5
J David Eisenberg <jdavid.eisenberg@gmail.com>
2018-01-16 17:19:14 -0800

    Commit a sub-project.

--- domavg/src/Main.purs
+++ domavg/src/Main.purs
@@ -1,80 +1,74 @@
 module Main where
 
-import Prelude
+import Prelude (Unit, bind, pure, show, unit, (+), (/))
+import Control.Apply (lift2)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
-import Data.Maybe (Maybe(..), fromMaybe)
+import Control.Monad.Eff.Console (CONSOLE)
+import Data.Maybe (Maybe(..))
 import Data.Number (fromString)
 import DOM (DOM)
-import DOM.Node.Types (ElementId(..), Element, elementToEventTarget)
-import DOM.Node.NonElementParentNode (getElementById)
+import DOM.Node.Types (Element, elementToEventTarget)
 import DOM.HTML.HTMLInputElement (value)
-import DOM.HTML.Types(HTMLInputElement)
+import DOM.HTML.Types (HTMLInputElement)
 import DOM.HTML (window)
 import DOM.HTML.Window (document)
 import DOM.HTML.Document (body)
-import DOM.Node.Node (textContent, setTextContent)
 import DOM.Node.ParentNode (QuerySelector(..))
+import DOM.Classy.Node (setTextContent)
 import DOM.Classy.ParentNode (querySelector)
 import DOM.Event.EventTarget (addEventListener, eventListener)
-import DOM.Event.Types (Event(..), EventType(..), EventTarget(..))
-import DOM.Classy.Element (getAttribute, setAttribute, replaceChild, firstChild, setTextContent)
-import Partial.Unsafe (unsafePartial)
+import DOM.Event.Types (Event, EventType(EventType))
 import Unsafe.Coerce as U
-
-doStuff ::  (Maybe Element) -> Number
-doStuff (Just el) = 1.0
-doStuff _ = 0.0
 
 
 elementToHTMLInputElement :: Element -> HTMLInputElement
 elementToHTMLInputElement = U.unsafeCoerce
 
-getValue :: forall e. (Maybe Element) -> Eff (dom ::DOM | e) (Maybe String)
-getValue (Just el) = getAttribute "value" el
-getValue Nothing = pure Nothing
-
-
-getValueNum :: forall e. (Maybe Element) -> Eff (dom :: DOM | e) Number
-getValueNum (Just el) =
+getElementBySelector :: forall e. String -> Eff (dom :: DOM | e) (Maybe Element)
+getElementBySelector selector =
   do
-    valStr <- value (elementToHTMLInputElement el)
-    pure (fromMaybe 0.0 (fromString valStr))
-getValueNum _ = pure 0.0
-
-
-gv :: forall e. (Maybe Element) -> Eff (dom :: DOM | e) Number
-gv (Just el) =
-  value (elementToHTMLInputElement el) >>=
-  \valStr -> 
-      pure (fromMaybe 0.0 (fromString valStr))
-gv _ = pure 0.0
-
-
+    w <- window
+    d <- document w
+    bodyElement <- body d
+    result <- case bodyElement of
+      Just b ->
+        querySelector (QuerySelector selector) b
+      Nothing -> pure Nothing
+    pure result
+      
+getNumericValue :: forall e. String -> Eff (dom :: DOM, console :: CONSOLE | e) (Maybe Number)
+getNumericValue id =
+  do
+    selected <- getElementBySelector id
+    
+    case selected of
+      Just inputElement -> 
+        do
+          str <- (value (elementToHTMLInputElement inputElement))
+          pure (fromString str)
+      Nothing -> pure Nothing
+    
 respondToClick :: forall e. Event -> Eff (console :: CONSOLE, dom :: DOM | e) Unit
 respondToClick evt = do
-  w <- window
-  d <- document w
-  b <- body d
-
-  input1 <- case b of
-    Just el -> querySelector (QuerySelector "#input1") el
-    Nothing -> pure Nothing
-  x <- gv input1
-  logShow x
-  pure unit
+  textSpan <- getElementBySelector "#output"
+  value1 <- getNumericValue "#input1"
+  value2 <- getNumericValue "#input2"
+  let avg = lift2 (/) (lift2 (+) value1 value2) (Just 2.0)
+  let avgText =
+        case avg of
+          Just result -> (show result)
+          Nothing -> ""
+  case textSpan of
+    Just tspan ->
+      setTextContent avgText tspan
+    Nothing ->
+      pure unit
 
 main :: forall e. Eff (console :: CONSOLE, dom :: DOM | e) Unit
 main = do
-  w <- window
-  d <- document w
-  b <- body d
-
-  button <- case b of
-    Just el -> querySelector (QuerySelector "#calculate") el
-    Nothing -> pure Nothing
-  case  button of
-    Just b -> addEventListener (EventType "click")
+  button <- getElementBySelector "#calculate"
+  case button of
+    Just btn -> addEventListener (EventType "click")
                 (eventListener respondToClick)
-                false (elementToEventTarget b)
+                false (elementToEventTarget btn)
     Nothing -> pure unit
